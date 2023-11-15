f0c189e096c22ca3c8ed7cf5d530fe6d48c9224d
marsel <mariothen3@gmail.com>
2023-11-15 15:33:31 +0900

    tambah variabel.php

---
+++ variabel.php
@@ -0,0 +1,15 @@
+<?php
+// contoh penulisan variabel pada php
+
+$title = "sacode";
+$tag_line = "Papua coding school";
+$tahun ="2023";
+
+// menampilkan data variabel
+
+echo $title;
+echo "<br>";
+echo $tag_line;
+echo "<br>";
+echo $tahun;
+?>
