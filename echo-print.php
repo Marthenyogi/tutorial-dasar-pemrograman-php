372bf388e3433237b5ff3479858542167366269d
marsel <mariothen3@gmail.com>
2023-11-15 15:39:56 +0900

    tambah echo-print.php

---
+++ echo-print.php
@@ -0,0 +1,10 @@
+<?php
+
+// contoh penggunaan echo
+echo "<h1> papua tech community";
+echo "<br>";
+echo "<p>Berbagi dan belajar bersama di komunitas.";
+// contoh penggunaan print
+print "<h1>Papua coding School";
+print "<p> belajar coding dari dasar";
+?>
