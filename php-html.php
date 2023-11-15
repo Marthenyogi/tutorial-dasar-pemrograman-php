0409cf3ba63f29d146f6d725f30c43f3357efe61
marsel <mariothen3@gmail.com>
2023-11-15 15:20:00 +0900

    tambah php-html.php

---
+++ php-html.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kombinasi PHP & HTML</title>
+</head>
+<body>
+    
+    <h1>Kombinasi html dan php</h1>
+
+    <?php
+        echo "hello php!!";
+    ?>
+
+</body>
+</html>
